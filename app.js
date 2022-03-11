4637b19d8196a51b05c491aab989821615b72aa2
leenahiq <97670794+leenahiq@users.noreply.github.com>
2022-03-11 19:05:24 +0000

    updated

--- app.js
+++ app.js
@@ -1,10 +1,69 @@
+//example 1
 const add = (num1,num2) =>{
     return num1 + num2;
 }
 
-
+//example 2
 const myArray = ["dave","steve","gary"];
 
-module.exports = {add,myArray}
+//activity 1 
+
+const getNotNull  = ()=>{
+    return 20
+}
+
+//activity 2
+const truthy = (num1,num2)=>{
+ if (num1 === num2){
+    return true;  
+ }else{
+    return false;
+ }
+  
+}
+
+//activity 3
+
+const obj = {
+    name:"leenah",
+    age:"forever young"
+
+}
+
+//activity 4
+const getItems = (array) =>{
+    return array.filter((e) =>e.length >= 6);
+}
+
+// activity 5 
+const convertNumberToString = (num) => {
+	return num.toString();
+};
+
+//activity 6
+const getPlanetFromOrder = (order) => {
+	const planets = [
+		'Mercury',
+		'Venus',
+		'Earth',
+		'Mars',
+		'Jupiter',
+		'Saturn',
+		'Uranus',
+		'Neptune',
+	];
+	return planets[order - 1];
+};
+
+
+
+
+module.exports = {add,myArray,truthy,obj,getNotNull,getItems,convertNumberToString,getPlanetFromOrder}
+
+
+
+
+
+
 
 
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,5 +1,8 @@
 
+
+
 const app = require ("../app.js");
+//example 1 test
 
 describe("should add two numbers together",() =>{
     test("should equal 5 when passed 2 and 3",() =>{
@@ -12,6 +15,68 @@ describe("should add two numbers together",() =>{
 });
 
 
+//example 2 test 
+    
 it("shouls include the name dave",()=>{
     expect(app.myArray).toContain("dave")
+});
+
+
+//activity 1 
+test("should not return null",()=>{
+expect(app.getNotNull()).not.toBeNull();
+})
+
+//activity 2 test
+describe("should return true if 1 equal 1",()=>{
+    test("number 1 should be strictly equal to 1 should be true",()=>{
+        expect(app.truthy(1,1)).toBe(true)
+    });
+    test("number 1 should not be  equal to 2 should be false ",()=>{
+        expect(app.truthy(1,2)).toBe(false)
+    });
+
+    
+})
+
+
+//activity 3
+
+//activity 4 test 
+
+describe("should return item with 6 or more characters",()=>{
+    it("should return correct items given array of elements containing 6 or more characters",()=>{
+        const input = ['javascript', 'element', 'html', 'selector', 'css', 'DOM'];
+        expect(app.getItems(input)).toContain('javascript', 'selector', 'element');
+		expect(app.getItems(input)).not.toContain('html', 'css', 'DOM');
+
+    })
+})
+
+// activity 4 test
+it('should create object with correct properties', () => {
+	expect(app.createObject()).toMatchObject({
+        name:"leenah",
+        age:"forever young"
+    
+    });
+});
+
+
+
+// ativity 5 test
+it('should convert number to string', () => {
+	expect(app.convertNumberToString(17)).toBe('17');
+});
+
+
+//activity 6  
+describe('should display correct planet given order away from the Sun', () => {
+	it('should return Earth given 3rd planet from Sun', () => {
+		expect(app.getPlanetFromOrder(3)).toBe('Earth');
+	});
+
+	it('should return Mercury given 1st planet from Sun', () => {
+		expect(app.getPlanetFromOrder(1)).toBe('Mercury');
+	});
 });
